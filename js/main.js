e50dd7415e2ab2f08062db04f61e10a4beafcc64
Marina Nashaat <marinanashaat95@yahoo.com>
2022-02-03 01:32:27 +0200

    edit main.js

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ $(document).ready(function () {
     $(".nav-item > a[href^='#']").click(function () {
         let navLinkHref = $(this).attr("href");
         let sectionOffset = $(navLinkHref).offset().top;
-        $("html, body").animate({ scrollTop: sectionOffset }, 2000);
+        $("html, body").animate({ scrollTop: sectionOffset }, { queue: false, duration: 2000 });
         $(this).addClass("active");
         $(this).parent().siblings().find('a').removeClass('active')
     })
@@ -60,9 +60,9 @@ $(document).ready(function () {
         time: 2000
     })
 
-/* /////////loader page/////////// */
-$("#loader").fadeOut(1000,function(){
-    $("body").css("overflow-y","auto")
-});
+    /* /////////loader page/////////// */
+    $("#loader").fadeOut(1000, function () {
+        $("body").css("overflow-y", "auto")
+    });
 
 });
